c7e1a84a7b4200e127ac2e796795b999c3caade2
salemgrace <salem.bryant@gmail.com>
2019-01-05 21:30:21 -0500

    updating to test heroku

---
+++ client/build/precache-manifest.e1fa6dd9202fe30598b09b985a9e662e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1d76d5e3335b0f1d653",
+    "url": "/static/js/main.d1d76d5e.chunk.js"
+  },
+  {
+    "revision": "ca923c5207c5c126fca4",
+    "url": "/static/js/1.ca923c52.chunk.js"
+  },
+  {
+    "revision": "d1d76d5e3335b0f1d653",
+    "url": "/static/css/main.1ecb237d.chunk.css"
+  },
+  {
+    "revision": "64aa218bcf2e7a3b107f6244a5776e30",
+    "url": "/index.html"
+  }
+];
--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Saved from "./components/pages/Saved";
 import Search from "./components/pages/Search";
-import NavBar from "./components/NavBar";
-import Wrapper from "./components/Wrapper";
+import NavBar from "./components/layout/NavBar";
+import Wrapper from "./components/layout/Wrapper";
 import "./App.css";
 
 function App() {
